93eef28c2fed9cd3c8912b74c153aa49f731d9ac
Theo <ramiepondar11@gmail.com>
2025-03-14 23:19:18 +0800

    fix: whitespaces

--- midterm/act4.py
+++ midterm/act4.py
@@ -6,12 +6,12 @@ def getMax(weights, values, capacity):
     items = [Item(w,v) for w,v in zip (weights, values)]
 
     items.sort(key = lambda item: item.value / item.weight, reverse = True)
-
+    
     maxVal = 0.0
 
     for item in items:
         if capacity >= item.weight:
-            maxVal += item.weight
+            maxVal += item.value
             capacity -= item.weight
         else:
             maxVal += (item.value / item.weight) * capacity
